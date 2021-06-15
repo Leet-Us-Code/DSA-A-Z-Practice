48fbb85615fb8dae3e669b1e4598be87caea4cdd
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-15 10:15:59 +0530

    Update Edit Distance.cpp

--- Level - 3/Dynamic Programming/Edit Distance.cpp
+++ Level - 3/Dynamic Programming/Edit Distance.cpp
@@ -33,3 +33,43 @@ public:
         return dp[m][n];
     }
 };
+
+//space optimized solution using two arrays
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        int n = word1.length();
+        int m = word2.length();
+        int dp[2][n+1];
+        //make the value of first row equal to the col number 
+        for(int i = 0; i <= n; i++)
+            dp[0][i] = i;
+        //same for the first column 
+        
+        //populate the rest of the 2d array 
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                //if the length of string 1 is zero then add all the chars 
+                //since this deals with col we will be doing it inside the for loop 
+                if(j == 0)
+                    dp[i%2][j] = i;
+                else if(word2[i-1] == word1[j-1])
+                    dp[i%2][j] = dp[(i-1)%2][j-1];
+                else 
+                {
+                    int replace = dp[(i-1)%2][j-1];
+                    int deleteChar = dp[(i-1)%2][j];
+                    int insertChar = dp[i%2][j-1];
+                    dp[i%2][j] = 1 + min(replace, min(deleteChar, insertChar));
+                }
+            }
+        }
+        return dp[m%2][n];
+    }
+};
