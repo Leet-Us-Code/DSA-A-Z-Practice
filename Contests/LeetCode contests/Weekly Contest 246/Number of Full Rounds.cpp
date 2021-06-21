73c5e65b854c3c315a7e6bcc66a20bf7f71debd3
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-21 08:27:40 +0530

    Update Number of Full Rounds.cpp

--- Contests/LeetCode contests/Weekly Contest 246/Number of Full Rounds.cpp
+++ Contests/LeetCode contests/Weekly Contest 246/Number of Full Rounds.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
-    int numberOfRounds(string s, string f) {
-        int start = 60 * stoi(s.substr(0, 2)) + stoi(s.substr(3)), finish = 60 stoi(f.substr(0, 2)) + stoi(f.substr(3));
-        if (start > finish) 
-            finish += 60 * 24; 
-        return max(0, finish / 15 - (start + 14) / 15);
+    int stoiV1(string str)
+    {
+        int res = 0;
+        int temp = 0;
+        for (int i = 0; i < str.length(); i++) {
+
+            temp = temp * 10 + (str[i] - '0');
+        }
+        return temp;
+    }
+    int numberOfRounds(string startTime, string finishTime) {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        string starth = startTime.substr(0, 2);
+        string endh = finishTime.substr(0, 2);
+        string startm = startTime.substr(3, 2);
+        string endm = finishTime.substr(3, 2);
+        int startHour = stoiV1(starth);
+        int endHour = stoiV1(endh);
+        int startMin = stoiV1(startm);
+        int endMin = stoiV1(endm);
+        int res = (endHour - startHour)*4 + 96 + ((endMin/15) - ((startMin + 14)/15));
+        return res%96;
     }
 };
