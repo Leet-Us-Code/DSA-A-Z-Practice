dbcfde6505929a2ee212aa0de9cc49333dfe2132
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-25 08:31:09 +0530

    Create Climbing Stairs.cpp

---
+++ Level - 3/Dynamic Programming/Climbing Stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        int dp[n+1];
+        if(n == 1 || n == 2)
+            return n;
+        dp[0] = 0; // not required just kept  it 
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
