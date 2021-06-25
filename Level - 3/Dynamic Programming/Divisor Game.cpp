fedae78765c2dda18113207148523e1e88c4f501
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-25 08:33:24 +0530

    Create Divisor Game.cpp

---
+++ Level - 3/Dynamic Programming/Divisor Game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        if(n%2 == 0)
+            return true;
+        else
+            return false;
+    }
+};
