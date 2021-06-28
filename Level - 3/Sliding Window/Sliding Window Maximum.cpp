0d56eeb937c239a05051e3f95eb2463a68282940
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-28 15:52:56 +0530

    Create Sliding Window Maximum.cpp

---
+++ Level - 3/Sliding Window/Sliding Window Maximum.cpp
@@ -0,0 +1,49 @@
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+//OM GAN GANAPATHAYE NAMO NAMAH 
+//JAI SHRI RAM 
+//JAI BAJRANGBALI 
+//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+vector<int> maxInWindow(vector<int> nums,int k) {
+        //first find the next greater element 
+        int nge[100000];
+        stack<int> s;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(s.empty())
+            {
+                s.push(i);
+            }
+            else 
+            {
+                while(!s.empty() && nums[i] > nums[s.top()])
+                {
+                    nge[s.top()] = i;
+                    s.pop();
+                }
+                s.push(i);
+            }
+        }
+        while(!s.empty())
+        {
+            nge[s.top()] = n;
+            s.pop();
+        }
+        vector<int> res;
+        int j = 0;
+        for(int i = 0; i <= n-k; i++)
+        {
+            if(j < i)
+            {
+                j = i;
+            }
+            while(nge[j] < i+k)
+            {
+                j = nge[j];
+            }
+            res.push_back(nums[j]);
+        }
+        return res;
+    }
