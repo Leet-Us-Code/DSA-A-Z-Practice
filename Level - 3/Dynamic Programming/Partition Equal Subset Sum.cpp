cec28aab9b436637e12cbaba557c7de9752dd733
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-07-05 11:16:55 +0530

    Update Partition Equal Subset Sum.cpp

--- Level - 3/Dynamic Programming/Partition Equal Subset Sum.cpp
+++ Level - 3/Dynamic Programming/Partition Equal Subset Sum.cpp
@@ -4,42 +4,40 @@ public:
         //OM GAN GANAPATHAYE NAMO NAMAH 
         //JAI SHRI RAM 
         //JAI BAJRANGBALI 
-        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BAHDRE NARAYANA
         int sum = 0;
-        int tar; 
         int n = nums.size();
         for(int i = 0; i < n; i++)
-        {
             sum += nums[i];
-        }
         if(sum%2 != 0)
             return false;
-        tar = sum/2;
-        bool dp[n+1][tar+1];
-        //first col will be true 
+        int target = sum/2;
+        bool dp[n+1][target+1];
+        //first column will be true
         for(int i = 0; i <= n; i++)
         {
             dp[i][0] = true;
         }
-        //first row except first element will be false 
-        for(int i = 1; i <= tar; i++)
+        //first row except first element will be false
+        for(int i = 1; i <= target; i++)
         {
             dp[0][i] = false;
         }
-        //populate the dp array 
         for(int i = 1; i <= n; i++)
         {
-            for(int j = 1; j <= tar; j++)
+            for(int j = 1; j <= target; j++)
             {
-                dp[i][j] = dp[i-1][j]; // not including 
-                if(nums[i-1] <= j)
+                if(j < nums[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
                 {
-                    bool x = dp[i-1][j-nums[i-1]];
-                    if(x)
-                        dp[i][j] = true;
+                    bool x = dp[i-1][j];
+                    int rem = j - nums[i-1];
+                    bool y = dp[i-1][rem];
+                    dp[i][j] = x|y;
                 }
             }
         }
-        return dp[n][tar];
+        return dp[n][target];
     }
 };
