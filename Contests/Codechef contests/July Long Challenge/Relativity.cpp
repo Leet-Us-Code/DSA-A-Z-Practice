80b3ed87922e7bc56c26f77010801b1705e68f42
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-07-12 21:17:32 +0530

    Create Relativity.cpp

---
+++ Contests/Codechef contests/July Long Challenge/Relativity.cpp
@@ -0,0 +1,18 @@
+//OM GAN GANAPATHAYE NAMO NAMAH 
+//JAI SHRI RAM 
+//JAI BAJRANGBALI 
+//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >>  t;
+    while(t--)
+    {
+        int c, g;
+        cin >> g >> c;
+        int h = (c*c)/(2*g);
+        cout << h << endl;
+    }
+}
