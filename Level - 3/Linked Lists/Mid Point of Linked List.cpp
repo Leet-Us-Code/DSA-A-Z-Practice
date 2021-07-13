a530994980f70124815f5e817e738780996a5719
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-07-13 18:14:20 +0530

    Create Mid Point of Linked List.cpp

---
+++ Level - 3/Linked Lists/Mid Point of Linked List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow =  slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
