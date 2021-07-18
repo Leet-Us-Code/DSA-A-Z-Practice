7db95d4106427ea82ca530ece1e2b4f5db50bd2a
HariHaran <haran465@gmail.com>
2021-07-18 19:14:08 +0530

    Create LinkedList Cycle - 1.cpp

---
+++ 60 Must Solve List/LinkedList Cycle - 1.cpp
@@ -0,0 +1,23 @@
+// Hari
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+};
