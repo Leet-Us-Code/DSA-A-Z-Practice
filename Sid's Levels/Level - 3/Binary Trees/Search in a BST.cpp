1992a9a4d7243c3e8385d67e836aabe51d41ed15
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-07-23 17:01:19 +0530

    Create Search in a BST.cpp

---
+++ Sid's Levels/Level - 3/Binary Trees/Search in a BST.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LASKHMI NARAYANA, BHADRE NARAYANA
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL)
+            return NULL;
+        if(root->val == val)
+            return root;
+        if(val < root->val)
+        {
+            return searchBST(root->left, val);
+        }
+        else
+        {
+            return searchBST(root->right, val);
+        }
+    }
+};
