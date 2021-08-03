a375176e667ad1884621bd04e62bc825f55b437f
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-03 16:56:12 +0530

    Update Longest Common Subsequence.cpp

--- Sid's Levels/DPP/DP/Longest Common Subsequence.cpp
+++ Sid's Levels/DPP/DP/Longest Common Subsequence.cpp
@@ -1,3 +1,31 @@
+//Recursive 
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int lcs(string s1, string s2, int m, int n)
+    {
+        if(m == 0 || n == 0)
+            return 0;
+        if(s1[m-1] == s2[n-1])
+            return lcs(s1, s2, m-1, n-1) + 1;
+        else
+        {
+            int x = lcs(s1, s2, m-1, n);
+            int y = lcs(s1, s2, m, n-1);
+            return max(x, y);
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        return lcs(text1, text2, m, n);
+    }
+};
+
+//DP 
 class Solution {
 public:
     //OM GAN GANAPATHAYE NAMO NAMAH 
@@ -5,29 +33,24 @@ public:
     //JAI BAJRANGBALI 
     //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        int lcs[m+1][n+1];
-        //1st row will be 0
-        for(int i = 0; i <= n; i++)
-        {
-            lcs[0][i] = 0;
-        }
-        //1st col will be 0 
-        for(int i = 0; i <= m; i++)
-        {
-            lcs[i][0] = 0;
-        }
+        int m = text1.length();
+        int n = text2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        //1st row and 1st column will be zero 
         for(int i = 1; i <= m; i++)
         {
             for(int j = 1; j <= n; j++)
             {
-                if(text2[i-1] == text1[j-1])
-                    lcs[i][j] = lcs[i-1][j-1] + 1;
+                if(text1[i-1] == text2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
                 else
-                    lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        return lcs[m][n];
+        return dp[m][n];
     }
 };
