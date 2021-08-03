0d089e540d9d23d685f7c4f0fb7f29ae6a780b98
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-03 20:07:39 +0530

    Update Edit Distance.cpp

--- Sid's Levels/DPP/DP/Edit Distance.cpp
+++ Sid's Levels/DPP/DP/Edit Distance.cpp
@@ -24,3 +24,44 @@ public:
         return editDistance(word1, word2, m, n);
     }
 };
+
+//DP 
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        //1st row m == 0 then the values will be n 
+        for(int i = 0; i <= n; i++)
+        {
+            dp[0][i] = i;
+        }
+        //1st column n == 0 then the values will be m
+        for(int i = 0; i <= m; i++)
+        {
+            dp[i][0] = i;
+        }
+        //populating the rest of the dp matrix 
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                {
+                    int insert = dp[i][j-1] + 1;
+                    int remove = dp[i-1][j] + 1;
+                    int replace = dp[i-1][j-1] + 1;
+                    dp[i][j] = min({insert, remove, replace});
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
