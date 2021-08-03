fc2f1bb535925c4286c73c35a820ee26b239610f
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-03 16:05:06 +0530

    Update Longest Increasing Subsequence.cpp

--- Sid's Levels/DPP/DP/Longest Increasing Subsequence.cpp
+++ Sid's Levels/DPP/DP/Longest Increasing Subsequence.cpp
@@ -1,3 +1,35 @@
+// Recursive solution 
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI  RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int lis(vector<int> nums, int ind, int &maxTillNow)
+    {
+        if(ind == 0)
+            return 1;
+        int maxEndingHere = 1;
+        for(int i = 0; i < ind; i++)
+        {
+            int rest = lis(nums, i, maxTillNow);
+            if(nums[i] < nums[ind])
+            {
+                maxEndingHere = max(maxEndingHere, rest + 1);
+            }
+        }
+        maxTillNow = max(maxTillNow, maxEndingHere);
+        return maxEndingHere;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int maxTillNow = 1;
+        int n = nums.size()-1;
+        int x =  lis(nums, n, maxTillNow);
+        return maxTillNow;
+    }
+};
+
+//DP (Bottom up)
 class Solution {
 public:
     //OM GAN GANAPATHAYE NAMO NAMAH 
@@ -6,28 +38,21 @@ public:
     //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> lis(n);
-        lis[0] = 1;
+        vector<int> lis(n, 1);
         for(int i = 1; i < n; i++)
         {
-            int res = INT_MIN;
+            lis[i] = 1;
             for(int j = 0; j < i; j++)
             {
                 if(nums[j] < nums[i])
                 {
-                    res = max(res, lis[j]);
+                    lis[i] = max(lis[i], lis[j]+1);
                 }
             }
-            if(res == INT_MIN)
-                lis[i] = 1;
-            else
-                lis[i] = res + 1;
         }
-        int maxEle = 1;
+        int maxEle = INT_MIN;
         for(int i = 0; i < n; i++)
-        {
             maxEle = max(maxEle, lis[i]);
-        }
         return maxEle;
     }
 };
