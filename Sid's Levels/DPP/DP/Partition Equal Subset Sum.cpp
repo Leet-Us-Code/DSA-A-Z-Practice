82206c6154007031b21b7c7bc7db7d5f93758cda
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-05 14:33:30 +0530

    Update Partition Equal Subset Sum.cpp

--- Sid's Levels/DPP/DP/Partition Equal Subset Sum.cpp
+++ Sid's Levels/DPP/DP/Partition Equal Subset Sum.cpp
@@ -28,3 +28,40 @@ public:
 };
 
 //DP (tabulation) 
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(sum%2 != 0)
+            return false;
+        int tar = sum/2;
+        int n = nums.size();
+        bool dp[n+1][tar+1];
+        //first col will be True 
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = true;
+        //1st row will be false except the first element 
+        for(int i = 1; i <= tar; i++)
+            dp[0][i] = false;
+        //remaining 
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= tar; j++)
+            {
+                dp[i][j] = dp[i-1][j]; // not including 
+                //including 
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = (dp[i][j])|(dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+        return dp[n][tar];
+    }
+};
