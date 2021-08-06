c92567a18b30c76b1b1eb6c0ab6a41a82daede11
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-06 14:47:48 +0530

    Update House Robber.cpp

--- Sid's Levels/DPP/DP/House Robber.cpp
+++ Sid's Levels/DPP/DP/House Robber.cpp
@@ -20,3 +20,27 @@ public:
         return helper(nums, n-1);
     }
 };
+
+//DP solution 
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int rob(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        else if(nums.size() == 1)
+            return nums[0];
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++)
+        {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+    }
+};
