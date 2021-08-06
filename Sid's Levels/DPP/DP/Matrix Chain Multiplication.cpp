c1ddefc292fabd162f3b5f1ef3b9583551272ab5
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-06 18:14:59 +0530

    Update Matrix Chain Multiplication.cpp

--- Sid's Levels/DPP/DP/Matrix Chain Multiplication.cpp
+++ Sid's Levels/DPP/DP/Matrix Chain Multiplication.cpp
@@ -25,3 +25,31 @@ public:
 };
 
 //DP 
+class Solution{
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int matrixMultiplication(int n, int arr[])
+    {
+        // code here
+        int dp[n][n]; // dp[i][j] = min operations for arr[i...j]
+        for(int i = 0; i < n; i++)
+        dp[i][i] = 0; // matrix of size 1
+        for(int l = 2; l < n; l++)
+        {
+            for(int i = 1; i < n-l+1; i++)
+            {
+                int j = i + l - 1;
+                dp[i][j] = INT_MAX;
+                for(int k = i; k < j; k++)
+                {
+                    int op = dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j];
+                    dp[i][j] = min(dp[i][j], op);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+};
