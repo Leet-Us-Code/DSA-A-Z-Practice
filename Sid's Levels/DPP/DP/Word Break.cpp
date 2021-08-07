6fe8978d61deb7b3b85d043f8bca274b8c980d10
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-07 17:38:10 +0530

    Update Word Break.cpp

--- Sid's Levels/DPP/DP/Word Break.cpp
+++ Sid's Levels/DPP/DP/Word Break.cpp
@@ -25,3 +25,42 @@ public:
 };
 
 //DP (tabulation)
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> wb(n+1, false); // wb[i] -> checks if s[0..i-1] can be broken correctly 
+        if(n == 0)
+            return true;
+        for(int i = 1; i <= n; i++)
+        {
+            //partitioning  
+            string s1 = s.substr(0, i);
+            //checking if s1 exists or not 
+            vector<string> :: iterator it = find(wordDict.begin(), wordDict.end(), s1);
+            if(wb[i] == false && it != wordDict.end())
+                wb[i] = true;
+            if(wb[i] == true)
+            {
+                if(i == n)
+                    return true;
+                for(int j = i+1; j <= n; j++)
+                {
+                    //remaining string partition 
+                    string s2 = s.substr(i, j-i);
+                    vector<string> :: iterator it1 = find(wordDict.begin(), wordDict.end(), s2);
+                    if(wb[j] == false && it1 != wordDict.end())
+                        wb[j] = true;
+                    if(j == n && wb[j] == true)
+                        return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
