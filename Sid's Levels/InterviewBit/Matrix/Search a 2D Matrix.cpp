ab222b7263bb2dbef9e8b82a2e75156ed41ec2d4
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-19 10:11:22 +0530

    Create Search a 2D Matrix.cpp

---
+++ Sid's Levels/InterviewBit/Matrix/Search a 2D Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: 
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LASKHMI NARAYANA, BHADRE NARAYANA
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int i = 0, j = n - 1;
+        while(i < m && j >= 0)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                j--;
+            else
+                i++;
+        }
+        return false;
+    }
+};
