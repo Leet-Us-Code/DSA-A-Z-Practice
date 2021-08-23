2e6ddaf8dfbbdb09c73dc5b24affc0c9f69d7c23
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-23 17:41:47 +0530

    Create Sqrt(x).cpp

---
+++ Sid's Levels/InterviewBit/Search and Sort/Sqrt(x).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int mySqrt(int x) {
+        if(x == 0)
+            return 0;
+        else if(x == 1)
+            return 1;
+        int l = 0, r = x;
+        int ans = -1;
+        while(r >= l)
+        {
+            int mid = (r + l)/2;
+            if(mid <= (x/mid))
+            {
+                ans = mid;
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
