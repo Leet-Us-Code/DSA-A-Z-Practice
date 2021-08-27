7161e9fc29f5f7d6e31e3d9625552a3d33b21f2d
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-27 16:58:43 +0530

    Update Palindrome String.cpp

--- Sid's Levels/InterviewBit/Strings/Palindrome String.cpp
+++ Sid's Levels/InterviewBit/Strings/Palindrome String.cpp
@@ -1,20 +1,31 @@
-class Solution{
-public:	
-	
-	//OM GAN GANAPATHAYE NAMO NAMAH 
-	//JAI SHRI RAM 
-	//JAI BAJRANGBALI 
-	//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
-	int isPlaindrome(string s)
-	{
-	    // Your code goes here
-	    int n = s.length();
-	    for(int i = 0; i < (n/2); i++)
-	    {
-	        if(s[i] != s[n-1-i])
-	        return false;
-	    }
-	    return true;
-	}
-
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    bool checkPal(string s)
+    {
+        int n = s.length();
+        for(int i = 0; i < n/2; i++)
+        {
+            if(s[i] != s[n-1-i])
+                return false;
+        }
+        return true;
+    }
+    bool isPalindrome(string s) {
+        string temp;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(isalpha(s[i]) || isdigit(s[i]))
+            {
+                if(isalpha(s[i]))
+                    temp.push_back(tolower(s[i]));
+                else 
+                    temp.push_back(s[i]);
+            }
+        }
+        return checkPal(temp);
+    }
 };
