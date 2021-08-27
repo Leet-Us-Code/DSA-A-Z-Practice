5702ed3e99fdc90629d79fb2e2426169c2af982c
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-27 18:10:00 +0530

    Create Remove Duplicates.cpp

---
+++ Sid's Levels/InterviewBit/Strings/Remove Duplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0; 
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            while(i < n-1 && nums[i] == nums[i+1])
+                i++;
+            nums[j++] = nums[i];
+        }
+        return j;
+    }
+};
