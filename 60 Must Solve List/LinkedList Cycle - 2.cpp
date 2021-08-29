99c4910ab6f7c9630a6df7cb14b1253daa88ea1c
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-08-29 21:47:31 +0530

    Update LinkedList Cycle - 2.cpp

--- 60 Must Solve List/LinkedList Cycle - 2.cpp
+++ 60 Must Solve List/LinkedList Cycle - 2.cpp
@@ -30,3 +30,46 @@ public:
         return nullptr; // no cycle
     }
 };
+
+//Sid 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LASKSHMI NARAYANA, BHADRE NARAYANA
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL)
+            return NULL;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        if(slow != fast || fast == NULL)
+            return NULL;
+        else 
+        {
+            slow = head;
+            while(slow != NULL && fast != NULL && slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+};
