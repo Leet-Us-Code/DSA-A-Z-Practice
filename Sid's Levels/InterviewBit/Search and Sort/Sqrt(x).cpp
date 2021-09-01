77cd34559ba1fd5abf85d771910700a5a2665e44
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-09-01 11:08:26 +0530

    Update Sqrt(x).cpp

--- Sid's Levels/InterviewBit/Search and Sort/Sqrt(x).cpp
+++ Sid's Levels/InterviewBit/Search and Sort/Sqrt(x).cpp
@@ -5,10 +5,8 @@ public:
     //JAI BAJRANGBALI 
     //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
     int mySqrt(int x) {
-        if(x == 0)
-            return 0;
-        else if(x == 1)
-            return 1;
+        if(x == 0 || x == 1)
+            return x;
         int l = 0, r = x;
         int ans = -1;
         while(r >= l)
@@ -20,9 +18,7 @@ public:
                 l = mid + 1;
             }
             else
-            {
                 r = mid - 1;
-            }
         }
         return ans;
     }
