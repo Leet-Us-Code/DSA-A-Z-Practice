2c4c6b4669fb0b536b6567616e54f4c73f0563f3
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-09-02 17:03:24 +0530

    Create Finding MSB in O(1).cpp

---
+++ Sid's Levels/InterviewBit/Bit Magic/Finding MSB in O(1).cpp
@@ -0,0 +1,13 @@
+//OM GAN GANAPATHAYE NAMO NAMAH 
+//JAI SHRI RAM 
+//JAI BAJRANGBALI 
+//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int rem = (int)(log(n)/log(2));
+    cout << pow(2, rem) << endl;
+}
