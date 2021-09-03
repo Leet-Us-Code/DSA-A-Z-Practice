49e45f32a180e8935906065b7ab3b9eac6c9ec4b
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-09-03 12:39:31 +0530

    Create Factorial.cpp

---
+++ Sid's Levels/InterviewBit/Recursion/Factorial.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+//OM GAN GANAPATHAYE NAMO NAMAH 
+//JAI SHRI RAM 
+//JAI BAJRANGBALI 
+//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    long long int factorial(int n){
+        //code here
+        if(n == 0 || n == 1)
+        return 1;
+        else
+        return n*factorial(n-1);
+    }
+};
