0b2e158dbbe187e11643e95c9cf922b3c147e5d6
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-09-07 07:33:38 +0530

    Update Subset Sum Problem.cpp

--- Sid's Levels/InterviewBit/Recursion/Subset Sum Problem.cpp
+++ Sid's Levels/InterviewBit/Recursion/Subset Sum Problem.cpp
@@ -1,13 +1,25 @@
-class Solution{   
+class Solution {
 public:
-    bool isSubsetSum(int N, int arr[], int sum){
-        // code here 
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    bool isPossible(vector<int> nums, int sum, int ind)
+    {
         if(sum == 0)
-        return true;
-        if(N == 0)
-        return false;
-        bool x = isSubsetSum(N-1, arr, sum);
-        bool y = isSubsetSum(N-1, arr, sum-arr[N-1]);
-        return x|y;
+            return true;
+        if(ind < 0)
+            return false;
+        bool inc = isPossible(nums, sum-nums[ind], ind-1);;
+        bool exc = isPossible(nums, sum, ind-1);
+        return inc|exc;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(sum%2 != 0)
+            return false;
+        return isPossible(nums, sum/2, nums.size()-1);
     }
 };
