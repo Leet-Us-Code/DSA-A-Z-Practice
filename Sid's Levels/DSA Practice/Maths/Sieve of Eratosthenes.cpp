1edb76c2344f6867cb9dcd18f9a6b4d0760c3655
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-08 10:34:24 +0530

    Create Sieve of Eratosthenes.cpp

---
+++ Sid's Levels/DSA Practice/Maths/Sieve of Eratosthenes.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    vector<int> sieveOfEratosthenes(int n)
+    {
+        // Write Your Code here
+        vector<bool> sieve(n+1, false); // if false indicates prime 
+        for(int i = 2; i*i <= n; i++)
+        {
+            if(sieve[i] == false)
+            {
+                for(int j = i*i; j <= n; j += i)
+                {
+                    sieve[j] = true;
+                }
+            }
+        }
+        vector<int> res;
+        for(int i = 2; i <= n; i++)
+        {
+            if(sieve[i] == false)
+            res.push_back(i);
+        }
+        return res;
+    }
+};
