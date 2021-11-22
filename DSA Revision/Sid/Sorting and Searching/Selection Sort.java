eca04d9ee365b6f3c6d1df1ab38b6e533759e956
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-22 10:28:15 +0530

    Create Selection Sort.java

---
+++ DSA Revision/Sid/Sorting and Searching/Selection Sort.java
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for(int i = 0; i < n-1; i++)
+       {
+          int minEle = arr[i];
+          int minInd = i;
+          for(int j = i+1; j < n; j++)
+          {
+              if(minEle > arr[j])
+              {
+                  minEle = arr[j];
+                  minInd = j;
+              }
+          }
+          int temp = arr[i];
+          arr[i] = arr[minInd];
+          arr[minInd] = temp;
+       }
+    }
+};
