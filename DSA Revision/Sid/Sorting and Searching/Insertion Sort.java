8b74eeab996b885266b8d4ae2b498091d3638621
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-22 10:47:50 +0530

    Update Insertion Sort.java

--- DSA Revision/Sid/Sorting and Searching/Insertion Sort.java
+++ DSA Revision/Sid/Sorting and Searching/Insertion Sort.java
@@ -8,21 +8,16 @@ class Solution
   public void insertionSort(int arr[], int n)
   {
       //code here
-      for(int i = 0; i < n-1; i++)
+      for(int i = 1; i < n; i++)
       {
-          int minEle = arr[i];
-          int minInd = i;
-          for(int j = i+1; j < n; j++)
+          int key = arr[i];
+          int pred = i - 1;
+          while(pred >= 0 && arr[pred] > key)
           {
-              if(minEle > arr[j])
-              {
-                  minEle = arr[j];
-                  minInd = j;
-              }
+              arr[pred+1] = arr[pred];
+              pred--;
           }
-          int temp = arr[i];
-          arr[i] = arr[minInd];
-          arr[minInd] = temp;
+          arr[pred+1] = key;
       }
   }
 }
