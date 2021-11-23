9ea298a5bd42b87da4c6c887f5fce19b2795c643
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-23 15:13:51 +0530

    Create Find the Frequency.java

---
+++ DSA Revision/Sid/Binary Search/Find the Frequency.java
@@ -0,0 +1,66 @@
+//OM GAN GANAPATHAYE NAMO NAMAH 
+//JAI SHRI RAM 
+//JAI BAJRANGBALI 
+//AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+class Solution{
+    int firstOccurrence(int[] a, int x)
+    {
+        int ind = -1; 
+        int l = 0, r = a.length-1; 
+        while(r >= l)
+        {
+            int mid = (r+l)/2; 
+            if(a[mid] == x)
+            {
+                if(mid > 0 && a[mid] == a[mid-1])
+                {
+                    r--;
+                }
+                else 
+                {
+                    return mid; 
+                }
+            }
+            else if(a[mid] > x)
+            r--;
+            else
+            l++;
+        }
+        return ind;
+    }
+    int lastOccurrence(int[] a, int x)
+    {
+        int ind = -1;
+        int l = 0, r = a.length-1; 
+        while(r >= l)
+        {
+            int mid = (r + l)/2;
+            if(a[mid] == x)
+            {
+                if(mid < a.length-1 && a[mid] == a[mid+1])
+                {
+                    l++;
+                }
+                else
+                {
+                    return mid; 
+                }
+            }
+            else if(a[mid] > x)
+            r--;
+            else
+            l++;
+        }
+        return ind;
+    }
+    int findFrequency(int[] a, int x){
+        Arrays.sort(a);
+        int first = firstOccurrence(a, x);
+        int last = lastOccurrence(a, x);
+        
+        if(first == -1 && last == -1)
+        return 0;
+        else 
+        return last-first+1;
+    }
+}
