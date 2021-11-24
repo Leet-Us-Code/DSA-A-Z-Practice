3c6579adcb1ce8ad44a3710003d10f646f51eaf9
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-24 17:28:48 +0530

    Create Factorial.java

---
+++ DSA Revision/Sid/Recursion and Backtracking/Factorial.java
@@ -0,0 +1,13 @@
+class Solution{
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    static long factorial(int n){
+        // code here
+        if(n == 0 || n == 1)
+        return 1;
+        else
+        return n*factorial(n-1);
+    }
+}
