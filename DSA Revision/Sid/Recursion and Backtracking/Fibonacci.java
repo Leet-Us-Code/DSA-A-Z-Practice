724475fd0d88715f45d737d53086b91666843335
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-11-24 17:35:55 +0530

    Create Fibonacci.java

---
+++ DSA Revision/Sid/Recursion and Backtracking/Fibonacci.java
@@ -0,0 +1,11 @@
+class Solution {
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+    public int fib(int n) {
+        if(n == 0 || n == 1)
+            return n;
+        return fib(n-1) + fib(n-2);
+    }
+}
