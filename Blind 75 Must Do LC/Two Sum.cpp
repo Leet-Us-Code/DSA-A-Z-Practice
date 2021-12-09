9c84139dc5ae5ae6a50b7f8a1daa5ed5c6fb8a24
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-12-09 14:24:20 +0530

    Update Two Sum.cpp

--- Blind 75 Must Do LC/Two Sum.cpp
+++ Blind 75 Must Do LC/Two Sum.cpp
@@ -14,3 +14,30 @@ vector<int> twoSum(vector<int>& nums, int target) {
         }
         return res;
     }
+
+// Sid -> Java solution 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> a = new HashMap<Integer, Integer>();
+        int n = nums.length;
+        //inserting all the elements in  the map 
+        for(int i = 0; i < n; i++)
+        {
+            a.put(nums[i], i);
+        }
+        int[] res = new int[2];
+        for(int i = 0; i < n; i++)
+        {
+            int rem = target - nums[i];
+            //check if the rem exists in the hashmap 
+            Integer ind = a.get(rem);
+            if(ind != null && ind != i)
+            {
+                res[0] = i; 
+                res[1] = ind;
+                break;
+            }
+        }
+        return res;
+    }
+}
