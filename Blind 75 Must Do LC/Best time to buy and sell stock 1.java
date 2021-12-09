d32b7764b26228a68b692dafaf3db833cf4a020d
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-12-09 23:01:35 +0530

    Update Best time to buy and sell stock 1.java

--- Blind 75 Must Do LC/Best time to buy and sell stock 1.java
+++ Blind 75 Must Do LC/Best time to buy and sell stock 1.java
@@ -23,3 +23,39 @@ class Solution {
         return maxProfit;
     }
 }
+
+//Sid - Method 1 
+class Solution {
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LASKHMI NARAYANA, BHADRE NARAYANA
+    public static int max(int a, int b)
+    {
+        if(a > b)
+            return a;
+        return b;
+    }
+    public static int min(int a, int b)
+    {
+        if(a > b)
+            return b;
+        return a;
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] minArr = new int[n];
+        int minEle = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++)
+        {
+            minEle = min(minEle, prices[i]);
+            minArr[i] = minEle;
+        }
+        int maxProfit = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++)
+        {
+            maxProfit = max(maxProfit, prices[i] - minArr[i]);
+        }
+        return maxProfit;
+    }
+}
