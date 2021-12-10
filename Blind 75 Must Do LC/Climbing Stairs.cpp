2ebc1cdee350f2430c7e7ad1c78f067f49c9d502
HariHaran <haran465@gmail.com>
2021-12-10 11:22:24 +0530

    Update Climbing Stairs.cpp

--- Blind 75 Must Do LC/Climbing Stairs.cpp
+++ Blind 75 Must Do LC/Climbing Stairs.cpp
@@ -1,4 +1,4 @@
-// hari
+// Hari
 
 int climbStairs(int n) {
         // fast
@@ -14,3 +14,21 @@ int climbStairs(int n) {
         }
         return dp[n];
     }
+
+// Hari - Java
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        // base
+        if(n == 0 || n == 1 || n == 2) return n;
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3; i<=n; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+}
