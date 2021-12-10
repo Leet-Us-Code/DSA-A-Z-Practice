288bc3893f9e59b120398ac2cc21c415cd282ce9
Millennial Pirate <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-12-10 11:26:43 +0530

    Update Climbing Stairs.cpp

--- Blind 75 Must Do LC/Climbing Stairs.cpp
+++ Blind 75 Must Do LC/Climbing Stairs.cpp
@@ -32,3 +32,27 @@ class Solution {
         return arr[n];
     }
 }
+
+//Sid 
+class Solution {
+    //OM GAN GANAPATHAYE NAMO NAMAH 
+    //JAI SHRI RAM 
+    //JAI BAJRANGBALI 
+    //AMME NARAYANA, DEVI NARAYANA, LASKHMI NARAYANA, BHADRE NARAYANA
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        if(n == 0 || n == 1 || n == 2)
+            return n;
+        arr[0] = 0; 
+        arr[1] = 1; 
+        arr[2] = 2; 
+        for(int i = 3; i <= n; i++)
+        {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+        // if(n == 0 || n == 1 || n == 2)
+        //     return n;
+        // return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
