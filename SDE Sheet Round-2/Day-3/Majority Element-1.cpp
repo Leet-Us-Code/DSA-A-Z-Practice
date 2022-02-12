443274e33d197804de02b2ff0c62c9e649c6288a
HariHaran <haran465@gmail.com>
2022-02-13 03:36:44 +0530

    Create Majority Element-1.cpp

---
+++ SDE Sheet Round-2/Day-3/Majority Element-1.cpp
@@ -0,0 +1,23 @@
+// Hari
+
+// Problem - Majority Element (>N/2 times)
+
+int majorityElement(vector<int>& nums) {
+        // fast
+        ios_base::sync_with_stdio(false); 
+        // Moore's voting algo - we pick an ele as majority ele, and inc it's count on every occurence. if a diff element is visited, decrease this count. 
+        // when count == 0, then change majority ele to curr ele, and repeat above steps. Final remaining majority ele is the ans. 
+        
+        int count = 0, majorityEle = 0;
+        for(auto it: nums){
+            if(count == 0){
+                majorityEle = it;
+            }
+            // if currEle same as majorityEle, inc count
+            if(it == majorityEle) count++;
+            // if they don't match, decrement
+            else count--;
+        }
+        
+        return majorityEle;
+    }
