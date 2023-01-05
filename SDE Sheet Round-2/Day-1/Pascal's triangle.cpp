86bcbde1f1b1aab4ecd88543304428f3fa352d5d
HariHaran <haran465@gmail.com>
2023-01-05 19:30:11 +0530

    Update Pascal's triangle.cpp

--- SDE Sheet Round-2/Day-1/Pascal's triangle.cpp
+++ SDE Sheet Round-2/Day-1/Pascal's triangle.cpp
@@ -18,3 +18,22 @@ vector<vector<int>> generate(int numRows) {
         
         return matrix;
     }
+
+// Hari
+
+vector<vector<int>> generate(int numRows) {
+        // fast
+        ios_base::sync_with_stdio(false);
+        vector<vector<int>> res;
+
+        for(int i = 0; i<numRows; i++){
+            vector<int> row(i+1, 1); 
+            // start from second index as first and last values will stay as 1
+            for(int j = 1; j<i; j++){
+                row[j] = res[i-1][j-1] + res[i-1][j];
+            }
+            res.push_back(row);
+        }
+
+        return res; 
+    }
