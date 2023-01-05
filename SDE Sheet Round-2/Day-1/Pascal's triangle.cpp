f1a8b05a4b73973b2290bee615cebe7d137b9051
Siddharth.S.Chandran <64896383+MillennialOptimizer@users.noreply.github.com>
2023-01-05 19:30:42 +0530

    Update Pascal's triangle.cpp

--- SDE Sheet Round-2/Day-1/Pascal's triangle.cpp
+++ SDE Sheet Round-2/Day-1/Pascal's triangle.cpp
@@ -37,3 +37,30 @@ vector<vector<int>> generate(int numRows) {
 
         return res; 
     }
+
+
+// java
+class Solution {
+    static Long mod = new Long(1000000007);
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        // code here
+        //JAI SHRI RAM 
+        Long[][] matrix = new Long[n][n]; 
+        for(int i = 0; i < n; i++){
+            Long[] row = new Long[i+1]; 
+            matrix[i][0] = new Long(1); 
+            matrix[i][i] = new Long(1); 
+            for(int j = 1; j < i; j++){
+                matrix[i][j] = (matrix[i-1][j-1]%mod + matrix[i-1][j]%mod)%mod; 
+            }
+        }
+        
+        ArrayList<Long> arr = new ArrayList<Long>();
+        
+        for(int i = 0; i < n; i++){
+            arr.add(new Long(matrix[n-1][i]));
+        }
+        
+        return arr;
+    }
+}
