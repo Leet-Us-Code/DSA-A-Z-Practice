4c6b2c38a886ade9ca684ca6fb6a52e87edf0462
HariHaran <haran465@gmail.com>
2023-06-07 23:21:14 +0530

    Create solution.cpp

---
+++ NeetCode-150/Two Pointers/Valid Palindrome/solution.cpp
@@ -0,0 +1,15 @@
+// Hari
+
+bool isPalindrome(string s) {
+        for(int i = 0, j = s.size()-1; i<j; i++, j--) {
+            while (i<j && !isalnum(s[i])) {
+                i++;
+            } 
+            while (i<j && !isalnum(s[j])) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) return false;
+        }
+        
+        return true;
+    }
